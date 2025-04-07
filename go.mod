bd4932a12350ad5ff88cb25962b52e9b2cef33f0
Joey Tan <j.tan2231@gmail.com>
2025-04-07 04:59:27 -0500

    removed sqlite stuff

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.24.1
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/mattn/go-sqlite3 v1.14.24
 )
 
 require (
--- main.go
+++ main.go
@@ -4,21 +4,15 @@ import (
 	"log"
 	"net/http"
 
-	"sanctum/database"
 	"sanctum/handlers"
 	"sanctum/middleware"
 )
 
 func main() {
-	err := database.InitDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	http.HandleFunc("/grade", middleware.LoggingMiddleware(middleware.AuthMiddleware(handlers.GradeHandler)))
 	http.HandleFunc("/add-card", middleware.LoggingMiddleware(middleware.AuthMiddleware(handlers.AddCardHandler)))
 	http.Handle("/remove-card", middleware.LoggingMiddleware(middleware.AuthMiddleware(handlers.RemoveCardHandler)))
-	
+
 	http.HandleFunc("/auth", middleware.LoggingMiddleware(handlers.AuthHandler))
 	http.HandleFunc("/generate-deck", middleware.LoggingMiddleware(middleware.AuthMiddleware(handlers.GenerateDeckHandler)))
 	http.HandleFunc("/prompt-suggestion", middleware.LoggingMiddleware(middleware.AuthMiddleware(handlers.PromptSuggestionHandler)))
